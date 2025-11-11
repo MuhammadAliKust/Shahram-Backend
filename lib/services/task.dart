ce30dc7333e75f7b067dfa927cf50c3db08aefb6
Muhammad Ali <kustalics@gmail.com>
2025-11-11 15:45:23 +0500

    Create Task, Update Task, Delete Task, Mark as Complete Done

---
+++ lib/services/task.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shahram_backend/models/task.dart';
+
+class TaskServices {
+  ///Create Task
+  Future createTask(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .add(model.toJson());
+  }
+
+  ///Update Task
+  Future updateTask(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(model.docId)
+        .update({'title': model.title, 'description': model.description});
+  }
+
+  ///Delete Task
+  Future deleteTask(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(model.docId)
+        .delete();
+  }
+
+  ///Mark Task as Complete
+  Future markTaskAsComplete(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(model.docId)
+        .update({'isCompleted': true});
+  }
+}
